968e7aba994e5a229261eb1af6fff35ea86449e7
Sitestree <maickelbecker705@gmail.com>
2025-06-17 17:19:33 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,14 +1,14 @@
 <?php
 $host = "localhost";
-$user = "root"; // Usuário padrão do MySQL
-$password = "senha123"; // Senha definida no Docker
-$dbname = "sistema"; // Nome do banco criado no Docker
+$user = "root"; // Usuário do MySQL no Docker
+$password = "senha123"; // Senha do MySQL no Docker
+$dbname = "sistema"; // Nome do banco de dados
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Erro de conexão: " . $conn->connect_error);
 } else {
-    echo "Conectado ao banco de dados no Docker!";
+    echo "Conectado ao banco de dados!";
 }
 ?>
